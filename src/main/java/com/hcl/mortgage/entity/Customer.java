25fc006a710b50d0a862dd2504b66f79fb719060
Jyoshna <moramjyoshna@gmail.com>
2019-10-11 04:14:55 +0530

    Changes made for some testcases added comments and loggers and reduced code smells

--- src/main/java/com/hcl/mortgage/entity/Customer.java
+++ src/main/java/com/hcl/mortgage/entity/Customer.java
@@ -18,46 +18,58 @@ public class Customer {
 	private String emailId;
 	private Long mobileNumber;
 	private LocalDate dateOfBirth;
-    private String password;
-	
+	private String password;
+
 	public Integer getCustomerId() {
 		return customerId;
 	}
+
 	public void setCustomerId(Integer customerId) {
 		this.customerId = customerId;
 	}
+
 	public String getCustomerName() {
 		return customerName;
 	}
+
 	public void setCustomerName(String customerName) {
 		this.customerName = customerName;
 	}
+
 	public String getEmailId() {
 		return emailId;
 	}
+
 	public void setEmailId(String emailId) {
 		this.emailId = emailId;
 	}
+
 	public Long getMobileNumber() {
 		return mobileNumber;
 	}
+
 	public void setMobileNumber(Long mobileNumber) {
 		this.mobileNumber = mobileNumber;
 	}
+
 	public LocalDate getDateOfBirth() {
 		return dateOfBirth;
 	}
+
 	public void setDateOfBirth(LocalDate date) {
 		this.dateOfBirth = date;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
 	public Customer() {
 		super();
 	}
-	
+
 }
